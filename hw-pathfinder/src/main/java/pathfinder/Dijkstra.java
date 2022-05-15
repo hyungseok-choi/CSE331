f11e14af4bc174d70a1652d9fb256477424b9dee
hyungseok-choi <hsuk1648@gmail.com>
2022-05-15 14:25:25 -0700

    [MOD] Dijkstra implementation (using given Path)

--- hw-pathfinder/src/main/java/pathfinder/Dijkstra.java
+++ hw-pathfinder/src/main/java/pathfinder/Dijkstra.java
@@ -1,6 +1,7 @@
 package pathfinder;
 
 import graph.Graph;
+import pathfinder.datastructures.Path;
 
 import java.util.*;
 
@@ -13,26 +14,32 @@ public class Dijkstra{
      * @param graph a graph with node, edges
      * @param srcNode starting node for dijkstra algorithm
      * @param dstNode destination node for dijkstra algorithm
-     * @throws RuntimeException when graph doesn't contain srcNode
+     * @throws NoSuchElementException when graph doesn't contain srcNode or dstNode
+     * @throws RuntimeException when there are no paths from srcNode to dstNode
      * @return a list of shortest path from srcNode to dstNode and total costs.
-     * @spec.requires Graph contains srcNode
+     * @spec.requires Graph contains srcNode and a path from srcNode to dstNode.
      */
     public static <T> Path<T> findPath(Graph<T, Double> graph, T srcNode, T dstNode){
 
-        if (!graph.listNodes().contains(srcNode)){
-            throw new RuntimeException("graph doesn't contain srcNode");
+        if (!graph.listNodes().contains(srcNode) || !graph.listNodes().contains(dstNode)){
+            throw new NoSuchElementException("graph doesn't contain Node");
         }
 
         T start = srcNode;
         T dest = dstNode;
-        PriorityQueue<Path<T>> active = new PriorityQueue<>();
+        PriorityQueue<Path<T>> active = new PriorityQueue<>(new Comparator<Path<T>>() {
+            @Override
+            public int compare(Path<T> o1, Path<T> o2) {
+                return Double.compare(o1.getCost(), o2.getCost());
+            }
+        });
         Set<T> finished = new HashSet<>();
 
-        active.add(new Path<>(new Graph.Edge<T, Double>(start, start, 0.0)));
+        active.add(new Path<T>(srcNode));
 
         while (!active.isEmpty()){
             Path<T> minPath = active.remove();
-            T minDest = minPath.pairs.get(minPath.pairs.size() - 1 ).getdstName();
+            T minDest = minPath.getEnd();
 
             if (dest.equals(minDest)){
                 return minPath;
@@ -45,39 +52,11 @@ public class Dijkstra{
 
             for (Graph.Edge<T, Double> child : graph.listChildren(minDest)) {
                 if (!finished.contains(child.getdstName())) {
-                    active.add(new Path<>(minPath, child));
+                    active.add(minPath.extend(child.getdstName(), child.getLabel()));
                 }
             }
             finished.add(minDest);
         }
-        return null;
-    }
-
-
-    /**
-     * immutable path class.
-     */
-    public static class Path<T> implements Comparable<Path<T>> {
-        private final List<Graph.Edge<T, Double>> pairs;
-        private final double totalCost;
-
-        public Path(Graph.Edge<T, Double> e){
-            List<Graph.Edge<T, Double>> tmp = new ArrayList<>();
-            tmp.add(e);
-            this.pairs = tmp;
-            this.totalCost = 0f;
-        }
-
-        public Path(Path<T> prev, Graph.Edge<T, Double> e){
-            List<Graph.Edge<T, Double>> tmp = new ArrayList<>(prev.pairs);
-            tmp.add(e);
-            this.pairs = tmp;
-            this.totalCost = prev.totalCost + e.getLabel();
-        }
-
-        @Override
-        public int compareTo(Path<T> o) {
-            return Double.compare(this.totalCost, o.totalCost);
-        }
+        throw new RuntimeException("no path found");
     }
 }
